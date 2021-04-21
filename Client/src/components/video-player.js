923107af586b655f0b245ce433a7cdb2c3b72c6c
tylermcarthur <tylermcarthur@yahoo.com>
2021-04-21 14:08:05 -0500

    ready for intergration

--- Client/src/components/video-player.js
+++
@@ -1,9 +0,0 @@
-const VideoPlayer = (props) =>{
-    return(
-        <video id='video-player' controls>
-        <source src={props.videoSrc} type='video/mp4'></source>
-      </video>
-    )
-}
-
-export default VideoPlayer
